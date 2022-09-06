076db39871d2f8642ae8898c4af5827fe1a1177d
Nicky van Urk <nickyvanurk@gmx.com>
2022-09-06 18:29:05 +0200

    Add triangle vertices and VBO

--- triangle/main.cpp
+++ triangle/main.cpp
@@ -26,6 +26,17 @@ int main() {
         return -1;
     }
 
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f, 0.5f, 0.0f,
+    };
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
 
