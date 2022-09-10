49865f75a1020a2d5d0ad01e285011f591c29378
Nicky van Urk <nickyvanurk@gmx.com>
2022-09-10 22:27:24 +0200

    Load container texture

--- textures/main.cpp
+++ textures/main.cpp
@@ -55,6 +55,23 @@ int main() {
 
     Shader ourShader("./shader.vs", "./shader.fs");
 
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
 
